1ea2af6cd18418464dafab0c0587e3d0514f5793
iamanujup <iamanujup79@gmail.com>
2026-09-10 23:24:08 +0530

    Update footer text to celebrate one year

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -28,7 +28,7 @@ export default function SpecialPersonApp() {
           duration: 0.8,
         }}
         className="fixed bottom-4 right-4 text-sm text-black/50 pointer-events-none z-50 font-light">
-        MADE BY NIKUNJ AND YOUR DAUGHTER NANDANI ❤️
+       ONE YEAR ❤️❤️❤️❤️
       </motion.div>
     </div>
   );
